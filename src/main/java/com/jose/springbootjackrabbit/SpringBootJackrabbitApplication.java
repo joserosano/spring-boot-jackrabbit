f121b4c7faa86e3b6b832f19bafb1da6a2a989b4
joserosano <joserosano86@gmail.com>
2018-04-10 16:50:53 +0200

    Initial commit

---
+++ src/main/java/com/jose/springbootjackrabbit/SpringBootJackrabbitApplication.java
@@ -0,0 +1,12 @@
+package com.jose.springbootjackrabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJackrabbitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJackrabbitApplication.class, args);
+    }
+}
